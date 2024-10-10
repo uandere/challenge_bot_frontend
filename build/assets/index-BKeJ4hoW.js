a4cf149846da5cc81138297aeb2c393dcf44f88c
Nazar Demchuk <nazar.demchvk@gmail.com>
2024-10-10 13:08:15 +0300

    Added build files

--- build/assets/index-BKeJ4hoW.js
+++ build/assets/index-BKeJ4hoW.js
@@ -1,4 +1,4 @@
-import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CgSs8DJG.js";import"./index-BkgRiHl0.js";/**
+import{T as xt,M as re,R as N,E as k,C as x,a as X,b as Wo,O as Z,c as ht}from"./index-CUsMU9BC.js";import"./index-Cu7p6ri3.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
